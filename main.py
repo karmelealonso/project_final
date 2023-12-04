0b68e85befafc51eb2022fca944555224c6407b0
Karmele Alonso Aia <karmele.alonso.aia@gmail.com>
2023-12-04 21:59:01 +0100

    en proceso

--- main.py
+++ main.py
@@ -1,67 +1,87 @@
-# Importar la biblioteca Streamlit
 import streamlit as st
 import pandas as pd
 
-#PRIMERO IMPORTO LOS DATOS:
 
-# Load the data
-df = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/obras_completo_para_estudio.csv")
-personajes_año_rango = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/personajes_año_rango_para_streamlit.csv")
+# Cargar los nuevos DataFrames
+personajes_streamlit = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/personajes_streamlit.csv")
+objetos_streamlit = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/objetos_streamlit.csv")
+fauna_streamlit = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/fauna_streamlit.csv")
+flora_streamlit = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/flora_streamlit.csv")
+lugar_streamlit = pd.read_csv("/Users/karmelealonsoaia/Desktop/ironhack_labs/PROYECTOS/project_final/data/data_clean/data_streamlit/lugar_streamlit.csv")
 
+# Obtener los 10 elementos más repetidos en cada categoría
+top_10_characters = ["Seleccionar Todas"] + personajes_streamlit['Personajes'].value_counts().head(10).index.tolist()
+top_10_objects = ["Seleccionar Todas"] + objetos_streamlit['Objetos'].value_counts().head(10).index.tolist()
+top_10_fauna = ["Seleccionar Todas"] + fauna_streamlit['Fauna'].value_counts().head(10).index.tolist()
+top_10_flora = ["Seleccionar Todas"] + flora_streamlit['Flora'].value_counts().head(10).index.tolist()
+top_10_place = ["Seleccionar Todas"] + lugar_streamlit['Lugar'].value_counts().head(10).index.tolist()
 
-# Llenar los valores nulos con 'Desconocidos' o 'Desconocida'
-df['Objetos'] = df['Objetos'].fillna('Desconocidos')
-df['Fauna'] = df['Fauna'].fillna('Desconocida')
-df['Lugar'] = df['Lugar'].fillna('Desconocido')
-personajes_año_rango['Personajes'] = df['Personajes'].fillna('Desconocidos')
+# Obtener todos los valores únicos en las categorías de Escuela y Año_rango
+unique_escuelas = personajes_streamlit['Escuela'].dropna().unique()
+unique_anio_rango = personajes_streamlit['Año_rango'].dropna().unique().tolist()
 
-# Filtrar categorías desconocidas
-df_filtered = df[
-    (df['Objetos'] != 'Desconocidos') &
-    (df['Fauna'] != 'Desconocida') &
-    (df['Lugar'] != 'Desconocido') &
-    (df['Personajes'] != 'Desconocidos')
-]
+# Ordenar los años en orden ascendente según la lista proporcionada
+orden_cronologico = ['(-400) - (-200)', '(-201) - 0', '1 - 200', '201 - 400', '401 - 600', '601 - 800', '801 - 1000', '1001 - 1200', '1201 - 1400', '1401 - 1600', '1601 - 1800', '1801 - 2000']
+unique_anio_rango = sorted(unique_anio_rango, key=lambda x: orden_cronologico.index(x))
 
-# Get the 10 most repeated elements in each category
-top_10_characters = personajes_año_rango['Personajes'].value_counts().head(10).index.tolist()
-top_10_objects = df_filtered['Objetos'].value_counts().head(10).index.tolist()
-top_10_fauna = df_filtered['Fauna'].value_counts().head(10).index.tolist()
-top_10_flora = df_filtered['Flora'].value_counts().head(10).index.tolist()
-top_10_place = df_filtered['Lugar'].value_counts().head(10).index.tolist()
+# Establecer los años mínimo y máximo
+min_year = personajes_streamlit['Año'].min()
+max_year = personajes_streamlit['Año'].max()
 
-# Estilo CSS
-st.markdown(
-    """
-    <style>
-    body {
-        color: #FAFAFA;
-        background-color: #848C8E;
-        font-family: sans-serif;
-    }
-    .stApp {
-        background-color: #848C8E;
-        color: #FAFAFA;
-    }
-    .stSelectbox, .stSlider, .stCheckbox, .stMultiselect {
-        background-color: #022B3A;
-        color: #FAFAFA;
-    }
-    .stTextInput, .stTextArea {
-        background-color: #022B3A;
-        color: #FAFAFA;
-    }
-    .stMarkdown {
-        color: #FAFAFA;
-    }
-    .stButton {
-        background-color: #FF8966;
-        color: #FAFAFA;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
+# Configuración del tema
+st.set_page_config(
+    page_title="Your App Name",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",  # Puedes cambiar esto a "collapsed" si prefieres que la barra lateral esté plegada inicialmente
 )
+
+# ESTILO DE LA APLICACIÓN
+
+CSS_STYLE_ = """
+<style>
+
+MainMenu {
+    visibility:hidden;
+}
+
+footer {
+    visibility:visible;
+}
+
+# Font family for all text in the app, except code blocks. One of "sans serif", "serif", or "monospace".
+font = "sans serif"
+
+h1 {
+    color: #FF8966;
+}
+h2 {
+    color: #848C8E;
+}
+h3 {
+    color: #022B3A;
+}
+h4 {
+    color: #FAFAFA;
+}
+
+.badge-custom {
+    color: #CCCCCC;
+    background-color: #0000CC;
+}
+
+button[data-baseweb="tab"] {
+    font-size: 18px;
+    color: #FE4A4A;
+}
+
+</style>
+
+<link rel="stylesheet" href="https://m axcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+"""
+
 
 # Crear el menú lateral
 page = st.sidebar.radio("", ["Home", "Analytical Studies", "Timeline"])
@@ -83,6 +103,7 @@ if page == "Home":
     st.markdown("## Inclusion criteria")
     # Añadir texto e imágenes para la sección de Inclusion criteria
 
+
 elif page == "Analytical Studies":
     # Crear el submenú para Analytical Studies
     sub_page = st.sidebar.radio("", ["Analytical Studies", "Characters", "Objects", "Fauna", "Flora", "Places"])
@@ -91,19 +112,31 @@ elif page == "Analytical Studies":
         st.title("Analytical Studies")
         st.markdown("Analytical studies")
 
-        # Define the sidebar menu for the "Analytical Studies" section
-        # Move the filters into the sidebar
+        # Definir el menú lateral para la sección "Analytical Studies"
+        # Mover los filtros al menú lateral
         selected_characters = st.sidebar.multiselect('Select characters', top_10_characters)
         selected_objects = st.sidebar.multiselect('Select objects', top_10_objects)
         selected_fauna = st.sidebar.multiselect('Select fauna elements', top_10_fauna)
         selected_flora = st.sidebar.multiselect('Select flora elements', top_10_flora)
         selected_places = st.sidebar.multiselect('Select place elements', top_10_place)
 
+        # Nuevo filtro para el rango de años
+        st.sidebar.markdown("### Select year range:")
+        selected_year_range = st.sidebar.slider(
+            label="",
+            min_value=-400,
+            max_value=2000,
+            value=(min_year, max_year),
+            step=1,
+            format="%d"
+        )
+
     elif sub_page == "Characters":
         st.title("Characters")
         st.markdown("Here you can explore the analytical studies of characters.")
         # Añadir componentes interactivos para los estudios analíticos de personajes
         # Por ejemplo, puedes usar st.selectbox, st.slider, st.checkbox, etc.
+        
 
     elif sub_page == "Objects":
         st.title("Objects")
@@ -123,4 +156,12 @@ elif page == "Analytical Studies":
     elif sub_page == "Places":
         st.title("Places")
         st.markdown("Analytical study of the places")
-        # Añadir contenido para la categoría de Places
+        selected_escuela_places = st.sidebar.multiselect('Select school', unique_escuelas)
+        selected_anio_rango_places = st.sidebar.mult
+
+    # Nuevo filtro para Escuela
+    selected_escuela = st.sidebar.multiselect('Select school', unique_escuelas)
+
+    # Nuevo filtro para Año_rango
+    selected_anio_rango = st.sidebar.multiselect('Select year range', unique_anio_rango)
+
